ef3603927613e6ffb530b4bfead2d87d05cd7690
Ben White <whtouche@gmail.com>
2014-06-27 10:06:18 -0400

    Create routes resources, more seed data, migration
    * Remember to do single-task commits in the future

--- db/migrate/20140626123057_create_bookmarks.rb
+++ db/migrate/20140626123057_create_bookmarks.rb
@@ -1,11 +1,11 @@
 class CreateBookmarks < ActiveRecord::Migration
   def change
     create_table :bookmarks do |t|
-      t.text :url
+      t.text :url, null: false
       t.text :title, null: false
       t.text :comment
       t.text :category
-      t.boolean :is_favorite, default: false
+      t.boolean :is_favorite null: false, default: false
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Bookmark.delete_all
+
+Bookmark.create!(
+  url: 'http://www.google.com',
+  title: 'Google!',
+  comment: 'My flavorite search engine',
+  category: 'Search',
+  is_favorite: true
+  )
+
+Bookmark.create!(
+  url: 'https://news.google.com',
+  title: 'Google News',
+  comment: 'My favorite news site',
+  category: 'News',
+  is_favorite: true
+  )
+
+Bookmark.create!(
+  url: 'https://www.facebook.com',
+  title: 'Facebook',
+  comment: 'Facebook comment',
+  category: 'Social',
+  is_favorite: true
+  )
+
+Bookmark.create!(
+  url: 'https://www.youtube.com',
+  title: 'YouTube',
+  comment: 'Youtube comment',
+  category: 'Videos',
+  is_favorite: false
+  )
+
+=begin
+Bookmark.create!(
+  url: ,
+  title: ,
+  comment: ,
+  category: ,
+  is_favorite:
+  )
+=end
