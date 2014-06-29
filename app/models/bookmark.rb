c72f30c82d30b448da3ce198697a8cad5ffe4d16
Ben White <whtouche@gmail.com>
2014-06-29 16:07:23 -0400

    Categories validation works, can add categories easily in one place

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,8 @@
 class Bookmark < ActiveRecord::Base
+
   validates :url, :format => URI::regexp(%w(http https))
   validates :url, presence: true
   validates :url, uniqueness: true
   validates :title, presence: true
+  validates :category, :inclusion => { :in => %w(Serious Funny Useful) }
 end
